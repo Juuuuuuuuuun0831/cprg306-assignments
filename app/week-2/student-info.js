b6ff963ae0efbf1b547e8bf7e92043882aa0eafa
Juuuuuuuuuun0831 <yunjunsang095@gmail.com>
2024-09-19 17:59:34 -0600

    Week 2 submission
    submitting week 2 assignments :)

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Jun Yun</p>
+            <Link href="https://github.com/Juuuuuuuuuun0831" legacyBehavior>
+            <a>Github Repository</a>
+            </Link>
+        </div>
+    );
+}
