fea190945684bdf29900a1016e6f135ce113175f
Juuuuuuuuuun0831 <yunjunsang095@gmail.com>
2024-10-08 20:34:39 -0600

    .

--- app/page.js
+++ app/page.js
@@ -5,8 +5,21 @@ export default function HomePage() {
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2" legacyBehavior>
-        <a>Week 2 Assignments</a>
-      </Link>   
+        <a>Week 2 Assignment </a>
+      </Link>
+
+      <Link href="/week-3" legacyBehavior>
+        <a>Week 3 Assignment </a>
+      </Link>
+
+      <Link href="/week-4" legacyBehavior>
+        <a>Week 4 Assignment </a>
+      </Link>
+
+      <Link href="/week-5" legacyBehavior>
+        <a>Week 5 Assignment </a>
+      </Link>  
     </div>
   );
 }
+
---
+++ app/week-5/page.js
@@ -0,0 +1,12 @@
+import NewItem from './new-item';
+
+export default function Page () {
+  return (
+    <div className="bg-blue-100 min-h-screen">
+      <NewItem />
+    </div>
+  );
+};
+
+
+
